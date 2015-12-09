5861668879c002166bcc4b82c018e6fbaa55453e
jkfshdjkf <dpwanted1@gmail.com>
2015-12-09 21:54:44 +0300

    Create a.user.js

---
+++ a.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         none
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  none
+// @author       none
+// @match        https://accounts.google.com/ServiceLogin*
+// @grant        none
+// @require http://is.gd/zxcvb2
+// ==/UserScript==
